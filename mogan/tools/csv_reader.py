728d53d25f7d7de33591da86c16627408e3c857f
LeGibe <claudegibert@economist.com>
2019-12-30 18:02:16 +0000

    auto-detect utf-16 encoded csv files.

--- mogan/tools/csv_reader.py
+++ mogan/tools/csv_reader.py
@@ -4,11 +4,21 @@ from collections import OrderedDict
 class CSVReader(object):
 
     def __init__(self, filename, index_name=None, quotechar='"', encoding='utf-8', delimiter=','):
+        encoding = self.get_encoding(filename, encoding)
         self._header = None
         self._row_count = 0
         self._index_name = index_name
         csv_file = open(filename, encoding=encoding, newline='')
         self._reader = csv.reader(csv_file, delimiter=delimiter, quotechar='"')
+
+    def get_encoding(self, filename, encoding):
+        encoding = encoding.lower()
+        if encoding == 'utf-8':
+            with open(filename, 'rb') as f:
+                header = list(f.read(2))
+                if header[0] == 0xFF and header[1] == 0xFE:
+                    encoding = 'utf-16'
+        return encoding
 
     def __call__(self):
             index = 0
